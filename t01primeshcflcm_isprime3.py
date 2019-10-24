d6871fb829697c2d607d886df6b19d9cc863ecfe
smexygoatsucks <52988324+smexygoatsucks@users.noreply.github.com>
2019-10-24 14:51:01 +0800

    ong xinhui 1f

--- t01primeshcflcm_isprime3.py
+++ t01primeshcflcm_isprime3.py
@@ -3,4 +3,11 @@
 # Consider implementing stop checking at int(num ** 0.5) for efficiency.
 
 num = 61
+num = 61
 
+prime = True
+for i in range(2,num): # won't work for 2 which is the only even prime
+  if num % i == 0:
+    prime = False
+  
+print(prime)
