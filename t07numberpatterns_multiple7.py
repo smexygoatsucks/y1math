f6ba70c21ed5fea60203ac49e7f442f015bb75b1
smexygoatsucks <52988324+smexygoatsucks@users.noreply.github.com>
2019-10-24 14:56:00 +0800

    ong xinhui 1f

--- t07numberpatterns_multiple7.py
+++ t07numberpatterns_multiple7.py
@@ -1,9 +1,8 @@
 # Generate multiples of 7 from 1 to 100
 
-START = 
-END = 
-num = 
+START = 1
+END = 100
+num = 7
 
 for i in range(START, END+1):
-  if :
-    print(i, end=' ')
+    print(num**i, end=' ')
